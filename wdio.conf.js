ac12f8ce35e3e6ecf9814e2521fb3bf237982b2e
hieuTranEnouvo <hieu.tran@enouvo>
2022-03-14 23:27:21 +0700

    remove docker for wdio.config.js file

--- wdio.conf.js
+++ wdio.conf.js
@@ -31,10 +31,7 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    runner: 'local',
-    hostname: 'localhost',
-    port: 4444,
-    path: '/',
+
     specs: [
         './features/**/*.feature'
     ],
@@ -74,24 +71,6 @@ exports.config = {
         //
         browserName: 'chrome',
         acceptInsecureCerts: true,
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
-        'goog:chromeOptions': {
-            // to run chrome headless the following flags are required
-            // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-            args: ['--headless', '--disable-gpu', '--no-sandbox', '--window-size=1920,1080', '--disable-dev-shm-usage']
-        }
-    },
-    {
-
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'firefox',
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
